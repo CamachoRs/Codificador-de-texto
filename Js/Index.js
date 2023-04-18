7012b69d4eba5f6c33771e4f83d1be1bd5bdff1a
Sebastian Camacho <juansebastiancamachoreyes084gmail.com>
2023-04-18 13:48:21 -0500

    Primer challenge terminado (segundo intento)

--- Js/Index.js
+++ Js/Index.js
@@ -1,11 +1,3 @@
-/*
-La letra "e" es convertida para "enter"
-La letra "i" es convertida para "imes"
-La letra "a" es convertida para "ai"
-La letra "o" es convertida para "ober"
-La letra "u" es convertida para "ufat"
-*/
-
 let no_mensajes = document.getElementById("no-mensaje");
 let si_mensajes = document.getElementById("si-mensaje");
 si_mensajes.style.display = "none";
